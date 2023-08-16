7a29399c41a6426e728a9b7af7cd71654312ffdb
gerardlonzi <lonzigerard@gmail.com>
2023-08-16 14:38:45 +0200

    poster les donnees sur sheets avec sheety Api

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
+import datetime as dt
+import os
+
+#post data to nutritionix Api
 API_KEY = 'b7fd2c8630ec91319955073b919885c5	'
 API_ID = '9c42f0be'
-url = ' https://trackapi.nutritionix.com/v2/natural/exercise'
+url_nutri_api = ' https://trackapi.nutritionix.com/v2/natural/exercise'
 exercise_text = input("Tell me which exercises you did: ")
 
-header ={
+header ={   
     'x-app-id':API_ID,
     'x-app-key':API_KEY
 }
@@ -17,6 +21,31 @@ params_nutri_Api = {
     'age': 20
     
 }
-requette = requests.post(url=url ,json=params_nutri_Api,headers=header)
-print(requette.json())
+requette = requests.post(url=url_nutri_api ,json=params_nutri_Api,headers=header)
+render = requette.json()
+print(render)
 
+# https://api.sheety.co/phill/myWebsite/emails
+
+# add row to google sheet by sheety Api
+url_sheety_Api = 'https://api.sheety.co/GerardLonzi/database/workouts'
+
+
+now = dt.datetime.now()
+date_now = now.strftime("%x")
+Hour_now = now.strftime('%X')
+
+for exo in render['exercises']:
+    
+    params = {
+            'workouts':{
+                'date':date_now,
+                'time':Hour_now,
+                'exercise': exo['name'].title(),
+                'duration':exo['duration_min'],
+                'calories':exo['nf_calories']
+                }
+    }
+
+requette_to_sheety = requests.post(url_sheety_Api,json=params)
+print(requette_to_sheety.text)
